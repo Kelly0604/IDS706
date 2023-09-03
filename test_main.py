75abdc07a6cf778683fb6bf65ce2746196a8eadd
Kelly064 <hong22@emory.edu>
2023-09-03 12:31:30 -0400

    changes2

--- test_main.py
+++ test_main.py
@@ -5,5 +5,5 @@ from main import subtraction
 def test_subtraction():
     "testing the subtraction function in main.py"
     assert subtraction(5,5) == 0
-    assert subtraction (1-6) == -5
-    assert subtraction (6-1) == 5
+    assert subtraction (1,6) == -5
+    assert subtraction (6,1) == 5
