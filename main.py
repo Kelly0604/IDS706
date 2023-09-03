f644bc88e82cc73239f18d9aacc8e94d6395a3d3
Kelly064 <hong22@emory.edu>
2023-09-03 12:29:40 -0400

    changes

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-"""
-Main cli or app entry point
-"""
-
-from mylib.calculator import add
-import click
-
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
-    click.echo(add(a, b))
-    click.echo(add(a, b))
-
-
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def subtraction(x,y):
+    result = 0
+    result = x - y
+    return result
